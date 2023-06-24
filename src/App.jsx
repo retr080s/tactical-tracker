f1b158b4ed93edd9a6b25e10865fe1a6376c1996
retr080s <retr080s@protonmail.com>
2023-06-24 08:54:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="terminal">
+      <h1 className="t-terminal">The Tactical Terminal</h1>
+      <div className="t-select">
+        <h1 className="t-terminal">T1</h1>
+        <h1 className="t-terminal">T2</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
